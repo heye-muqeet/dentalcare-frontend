f838246f65dfb83cf91b3b75bed3392ca020fcac
Engr. Abdul Muqeet <engr.abdulmuqeet42501@gmail.com>
2025-03-22 08:40:36 +0500

    feat: add centralized image and icon management with new components for display

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,29 @@
+// Import images using ES module import syntax
+import user from './icons/user.png';
+import users from './icons/users.png';
+import bar_chart from './icons/bar_chart.png';
+import calender_add from './icons/calender_add.png';
+import document_chart from './icons/document_chart.png';
+import logout from './icons/logout.png';
+import settings from './icons/settings.png';
+import vase from './icons/vase.png';
+import my_image from './images/my_image.png';
+
+// Export the images as named exports
 const icons = {
-    home: require("./icons/home.png"),
-    user: require("./icons/user.png"),
-    settings: require("./icons/settings.png"),
-    logout: require("./icons/logout.png"),
-  };
-  
-  const images = {
-    logo: require("./images/logo.png"),
-    banner: require("./images/banner.jpg"),
-    background: require("./images/background.png"),
-  };
-  
-  export { icons, images };
-  
+  user,
+  users,
+  bar_chart,
+  calender_add,
+  document_chart,
+  logout,
+  settings,
+  vase,
+};
+
+const images = {
+  my_image,
+};
+
+// Exporting icons and images
+export { icons, images };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import bar_chart from "../../src/assets/icon/bar_chart.png";
-import calender_add from "../../src/assets/icon/calender_add.png";
-import vase from "../../src/assets/icons/vase.png";
-import users from "../../src/assets/icon/users.png";
-import document_chart from "../../src/assets/icons/document_chart.png";
-import user from "../../src/assets/icon/user.png";
-import setting from "../../src/assets/icon/setting.png";
-import logout from "../../src/assets/icon/logout.png";
+import { icons } from "../assets";
+import Icon from "../components/Icons";
+
+interface MenuItemProps {
+  icon: keyof typeof icons; // 'icon' will be a key from the 'icons' object
+  text: string;
+}
+
 
 function Home() {
   return (
@@ -18,33 +18,33 @@ function Home() {
 
       {/* Menu Items */}
       <div className="space-y-1">
-        <MenuItem icon={bar_chart} text="Dashboard" />
-        <MenuItem icon={calender_add} text="Doctor Appointment" />
-        <MenuItem icon={vase} text="Lab Appointment" />
-        <MenuItem icon={users} text="Patients List" />
+        <MenuItem icon="bar_chart" text="Dashboard" />
+        <MenuItem icon="calender_add" text="Doctor Appointment" />
+        <MenuItem icon="vase" text="Lab Appointment" />
+        <MenuItem icon="users" text="Patients List" />
 
         <div className="bg-gray-200 h-0.5 w-full my-2"></div>
 
-        <MenuItem icon={document_chart} text="Billing" />
-        <MenuItem icon={user} text="Account" />
+        <MenuItem icon="document_chart" text="Billing" />
+        <MenuItem icon="user" text="Account" />
 
         <div className="bg-gray-200 h-0.5 w-full my-2"></div>
 
-        <MenuItem icon={setting} text="Settings" />
-        <MenuItem icon={logout} text="Log Out" />
+        <MenuItem icon="settings" text="Settings" />
+        <MenuItem icon="logout" text="Log Out" />
       </div>
     </div>
   );
 }
 
 // Reusable Menu Item Component
-function MenuItem({ icon, text }) {
+function MenuItem({ icon, text }: MenuItemProps) {
   return (
     <div
       className="flex items-center space-x-3 p-3 rounded-lg cursor-pointer 
       text-gray-700 text-[13px] font-medium hover:bg-blue-100 transition-all duration-200"
     >
-      <img src={icon} className="w-4 h-4" />
+      <Icon name={icon} width="w-4" height="h-4" />
       <span>{text}</span>
     </div>
   );
